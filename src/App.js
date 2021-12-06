6a2cfe16360b538ba84b877ed0233bf5495eb5ab
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-12-06 13:56:51 -0600

    Added Logo, Image Element, and About the Author element

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import logoGif from "./moneyrooklogo.gif";
+import imageOne from "./IMG_3715.jpeg";
+import imageTwo from "./IMG_3718.jpeg";
+import imageThree from "./IMG_3725.jpeg";
+import imageFour from "./IMG_4062.jpeg";
+import Video from "./Video.js"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="MoneyrookLogoHeader">
+        <img src={logoGif} alt="" length={190} width={190}></img>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6" id="BioSection">
+            <h2>About the artist</h2>
+            Biography section -Hometown -Age -Written in first-person or
+            third-person? -Facts/hobbies? -Projects, upcoming projects? Release
+            dates? -Possible merch?
+          </div>
+
+          <div className="col-6">
+            <div id="Images">
+              <div className="container">
+                <div className="row">
+                  <div className="col-3">
+                    <img src={imageOne} alt="" length={120} width={150}></img>
+                  </div>
+                  <div className="col-3">
+                    <img src={imageTwo} alt="" length={120} width={150}></img>
+                  </div>
+                  <div className="col-3">
+                    <img src={imageThree} alt="" length={120} width={150}></img>
+                  </div>
+                  <div className="col-3">
+                    <img src={imageFour} alt="" length={120} width={150}></img>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Video />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Video.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+export default function Video() {
+  return (
+    <div className="YoutubeVideoPlayer">
+      <iframe
+        id="player"
+        type="text/html"
+        width="640"
+        height="390"
+        src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+}
